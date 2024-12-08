03c0055fddfcb076d82c64f2325daa553370de15
Aleksei Lezhoev <lezhoev@gmail.com>
2024-12-08 13:29:31 +0100

    Make layers count keymap dependant for keycapsss/3w6_2040

---
+++ keyboards/keycapsss/3w6_2040/keymaps/default/config.h
@@ -0,0 +1,7 @@
+// Copyright 2021 weteor | 2022 Conor Burns (@Conor-Burns)
+// Copyright 2023 Ben Roe (@keycapsss)
+// Copyright 2023 Tom Barnes (@keyboard-magpie)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
